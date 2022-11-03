3497dd2771fcf80999b0f0066f9db1c04d9c67bd
charlybarrio <5757@holbertonstudents.com>
2022-11-03 07:05:48 -0700

    main

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+int _printf(const char *format, ...);
+#endif
