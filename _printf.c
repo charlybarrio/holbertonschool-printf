873e07ca12a01e5e29aefb8b4b02928ea417c6a0
charlybarrio <5757@holbertonstudents.com>
2022-11-09 17:32:05 -0800

    Main function of _printf

---
+++ _printf.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * _printf - write an output according to a format
+ * @format: the characters to print or the flags.
+ * Return: the number of characters printed.i
+ * c: travel the input
+ * q: count the characters
+ */
+int _printf(const char *format, ...)
+{
+	int c = 0, q = 0;
+	va_list arg;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+	va_start(arg, format);
+	while (format[c] != '\0')
+	{
+		while (format[c] != '%' && format[c] != '\0')
+		{
+			_putchar(format[c]);
+			q++;
+			c++;
+		}
+		if (format[c] == '\0')
+			return (q);
+		f = select_func(&format[c + 1]);
+		if (f != NULL)
+		{
+			q += f(arg);
+			c += 2;
+			continue;
+		}
+		if (format[c + 1] == '\0')
+			return (-1);
+		if (format[c + 1] == '%')
+			c += 2;
+		else
+		_putchar(format[c]);
+	        q++;
+			c++;
+	}
+	va_end(arg);
+	return (q);
+}
